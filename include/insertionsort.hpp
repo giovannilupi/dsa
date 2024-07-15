e2065ce742f144d7e39a7ef1cea598c87b384fbc
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-07-15 13:48:27 +0200

    Add mergesort

--- include/insertionsort.hpp
+++ include/insertionsort.hpp
@@ -2,6 +2,7 @@
 #define INSERTIONSORT_H
 
 #include "alg_concepts.hpp"
+#include <concepts>
 
 namespace alg {
 
@@ -13,6 +14,19 @@ void insertionSort(T& container) {
     for (int i = 1; i < container.size(); ++i) {
         // We go backwards looking for the insert position
         for (int j = i; j && container[j-1] > container[j]; --j) {
+            std::swap(container[j-1], container[j]);
+        }
+    }
+}
+
+/**
+ * Insertion sort with a custom low and high index.
+ * The high index is inclusive.
+ */
+template <SortableContainer T, std::integral Q>
+void insertionSort(T& container, Q low, Q high) {
+    for (Q i = 1; i <= high; ++i) {
+        for (Q j = i; j && container[j-1] > container[j]; --j) {
             std::swap(container[j-1], container[j]);
         }
     }
---
+++ include/mergesort.hpp
@@ -0,0 +1,96 @@
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+#include "alg_concepts.hpp"
+#include "insertionsort.hpp"
+#include <concepts>
+#include <vector>
+
+namespace alg {
+
+namespace detail {
+
+/**
+ * Merge function for divide and conquer implementation of merge sort.
+ * Merges two sorted subarrays into a single sorted array.
+ */
+template <SortableContainer T, std::integral Q>
+void merge(T& container, Q low, Q mid, Q high) {
+    Q i = low, j = mid+1;
+    // Temporary vector where to store the sorted unified container
+    std::vector<typename T::value_type> tmp;
+    while (i <= mid && j <= high) {
+        if (container[j] < container[i]) tmp.push_back(container[j++]);
+        else tmp.push_back(container[i++]);
+    }
+        // Add the remaining elements to the result (only one subarray can have elements left)
+        while (i <= mid) tmp.push_back(container[i++]);
+           while (j <= high) tmp.push_back(container[j++]);
+    // Copy tmp to the original container
+    for (Q idx = low; idx <= high; ++idx) container[idx] = tmp[idx-low]; 
+}
+
+/**
+ * Helper function for divide and conquer implementation of merge sort.
+ */
+template <SortableContainer T, std::integral Q>
+void mergeSortHelper(T& container, Q low, Q high) {
+    if (low < high) {
+        Q mid = low + (high - low) / 2;
+        mergeSortHelper(container, low, mid);
+        mergeSortHelper(container, mid+1, high);
+        merge(container, low, mid, high);
+    }
+}
+
+/**
+ * Helper function for coarse merge sort.
+ * We sort each subarray directly when its size is <= 10.
+ */
+template <SortableContainer T, std::integral Q>
+void mergeSortCoarseHelper(T& container, Q low, Q high) {
+    static const Q k = 10;
+    // The problem is simple enough to solve directly
+    if ((high - low) < k) insertionSort(container, low, high);
+    // Otherwise, divide and conquer
+    else {
+        Q mid = low + (high - low) / 2;
+        mergeSortHelper(container, low, mid);
+        mergeSortHelper(container, mid+1, high);
+        merge(container, low, mid, high);
+    }
+}
+
+}
+
+/**
+ * Generic implementation of the classic merge sort algorithm.
+ * Recurrence relation: T(n) = 2T(n/2) + \Theta(n)
+ * Complexity: O(n log n)
+ */
+template <SortableContainer T>
+void mergeSort(T& container) {
+    std::integral auto sz = container.size();
+    if (!sz) return;
+    decltype(sz) high = container.size() - 1;
+    decltype(sz) low = 0;
+    detail::mergeSortHelper(container, low, high);
+}
+
+/**
+ * Merge sort implementation using coarsening to limit the depth of recursion.
+ * We solve a subproblem directly when its size is <= k.
+ */
+template <SortableContainer T>
+void mergeSortCoarse(T& container) {
+    std::integral auto sz = container.size();
+    if (!sz) return;
+    decltype(sz) high = container.size() - 1;
+    decltype(sz) low = 0;
+    detail::mergeSortCoarseHelper(container, low, high);
+}
+
+}
+
+#endif // !MERGESORT_H
+
