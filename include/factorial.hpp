b7a3d5b911b3b0e1463112e68b8256553ac6edae
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-07-17 19:28:55 +0200

    Add factorial to include dir

--- include/factorial.hpp
+++ include/factorial.hpp
@@ -1,13 +1,18 @@
-#include <concepts>
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
 
-using namespace std;
+#include <__concepts/arithmetic.h>
+#include <concepts>
+#include <cstdint>
+
+namespace alg {
 
 /**
  * Naive recursive implementation.
  * The size of the call stack grows linearly with the size of n. 
  */
-template <integral T>
-long factRec(T n) {
+template <std::unsigned_integral T>
+uint64_t factRec(T n) {
     if (n == 0 || n == 1) return 1;
     return n * factRec(n-1);
 }
@@ -16,8 +21,8 @@ long factRec(T n) {
  * Tail recursive implementation.
  * This uses a second parameter to accumulate the factorial value.
  */
-template <integral T>
-long factRecTail(T n, T accum = 1) {
+template <std::unsigned_integral T>
+uint64_t factRecTail(T n, T accum = 1) {
     if (n == 0 || n == 1) return accum;
     return factRecTail(n-1, n * accum);
 }
@@ -25,9 +30,14 @@ long factRecTail(T n, T accum = 1) {
 /**
  * Iterative implementation.
  */ 
-template <integral T>
-long factIter(T n) {
-    long res = 1;
+template <std::unsigned_integral T>
+uint64_t factIter(T n) {
+    uint64_t res = 1;
     while(n) res *= n--;
     return res;
 } 
+
+}
+
+#endif // !FACTORIAL_H
+
