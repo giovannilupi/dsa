d346caa1190f872417d979c6efa0736ab9e8fd97
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-07-24 23:51:35 +0200

    Add common header
    As of now, it only contains the implementation of the container subscript type recommended by the C++ core guidelines.

---
+++ include/common.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+
+namespace alg {
+    /**
+     * Index type for subscripting containers.
+     * As recommended by the C++ core guidelines, to avoid issues with 
+     * signed/unsigned mixtures, it is better to use signed integers of sufficient size.
+     * The current implementation of the index type from the guidelines is a simple type alias.
+     */
+    using index = std::ptrdiff_t;
+}
