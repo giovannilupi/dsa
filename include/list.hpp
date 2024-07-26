edf6a3a7416cbcde23a0ee41a89eea17997128a7
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-07-26 16:50:39 +0200

    Add new functions to list header

--- include/list.hpp
+++ include/list.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <__concepts/equality_comparable.h>
+#include <__iterator/concepts.h>
+#include <cstddef>
+#include <initializer_list>
+#include <iostream>
+#include "alg_concepts.hpp"
+#include "common.hpp"
+
 namespace alg {
 
 /**
@@ -12,6 +20,177 @@ struct SListNode {
     SListNode(T val) : val(val), next(nullptr) {}
     SListNode(T val, SListNode* next) : val(val), next(next) {}
 };
+
+/**
+ * Reverses a singly linked list.
+ */
+template <typename T>
+SListNode<T>* reverseList(SListNode<T>* head) {
+    SListNode<T>* prev = nullptr;
+    while (head) {
+        SListNode<T>* next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
+/**
+ * Returns the length of a singly linked list.
+ * Complexity: O(n)
+ */
+template <typename T>
+std::size_t getListLen(SListNode<T>* head) {
+    std::size_t len = 0;
+    while (head) { ++len; head = head->next; }
+    return len;
+}
+
+/**
+ * Prints a singly linked list.
+ */
+template <PrintableElement T>
+void printList(SListNode<T>* head) {
+    while (head) {
+        std::cout << head->val;
+        head = head->next;
+        if (head) std::cout << " ";
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * Inserts an element into a singly linked list at a given position.
+ * If the position is out of bounds, the element is inserted at the end.
+ */
+template <typename T>
+SListNode<T>* insertList(SListNode<T>* head, std::size_t pos, const T& val) {
+    SListNode<T>* prev = nullptr;
+    SListNode<T>* curr = head;
+    index cpos = pos;
+    // Traverse to the position
+    while (cpos-- && curr) {
+        prev = curr;
+        curr = curr->next;
+    }
+    // Insert the element
+    SListNode<T>* new_node = new SListNode<T>(val, curr);
+    // Hanlde empty list
+    if (!prev) return new_node;
+    prev->next = new_node;
+    return head;
+}
+
+/**
+ * Prepends an element to the beginning of a singly linked list.
+ */
+template <typename T>
+SListNode<T>* insertListFront(SListNode<T>* head, const T& val) {
+    return insertList(head, 0, val);
+}
+
+/**
+ * Appends an element to the end of a singly linked list.
+ */
+template <typename T>
+SListNode<T>* insertListBack(SListNode<T>* head, const T& val) {
+    return insertList(head, getListLen(head), val);
+}
+
+/**
+ * Removes an element from a singly linked list at a given position.
+ * If the position is out of bounds, no element is removed.
+ */
+template <typename T>
+SListNode<T>* removeList(SListNode<T>* head, std::size_t pos) {
+    SListNode<T>* curr = head;
+    SListNode<T>* prev = nullptr;
+    index cpos = pos;
+    while (cpos-- && curr) {
+        prev = curr;
+        curr = curr->next;
+    }
+    if (!curr) return head;
+    SListNode<T>* nxt = curr->next;
+    delete(curr);
+    if (!prev) return nxt;
+    prev->next = nxt;
+    return head;
+}
+
+/**
+ * Removes an element from the beginning of a singly linked list.
+ */
+template <typename T>
+SListNode<T>* removeListFront(SListNode<T>* head) {
+    return removeList(head, 0);
+}
+
+/**
+ * Removes an element from the end of a singly linked list.
+ */
+template <typename T>
+SListNode<T>* removeListBack(SListNode<T>* head) {
+    std::size_t len = getListLen(head);
+    return len ? removeList(head, len - 1) : head;
+}
+
+/**
+ * Finds the first occurrence of an element in a singly linked list.
+ * If the element is not found, returns nullptr.
+ */
+template <std::equality_comparable T>
+SListNode<T>* findList(SListNode<T>* head, const T& val) {
+    while (head) {
+        if (head->val == val) return head;
+        head = head->next;
+    }
+    return nullptr;
+}
+
+/**
+ * Converts a singly linked list to a vector.
+ * Useful for testing.
+ */
+template <typename T>
+std::vector<T> listToVector(SListNode<T>* head) {
+    std::vector<T> vec;
+    while (head) {
+        vec.push_back(head->val);
+        head = head->next;
+    }
+    return vec;
+}
+
+/**
+ * Converts an iterator range to a singly linked list.
+ */
+template <typename T>
+SListNode<typename std::iterator_traits<T>::value_type>* toList(T first, T last) {
+    SListNode<typename std::iterator_traits<T>::value_type>* head = nullptr;
+    size_t cnt = 0;
+    while (first != last) head = insertList(head, cnt++, *first++);
+    return head;
+}
+
+/**
+ * Converts an initializer list to a singly linked list.
+ */
+template <typename T>
+SListNode<T>* genList(std::initializer_list<T> initList) {
+    return toList(initList.begin(), initList.end());
+}
+
+template <typename T>
+void deleteList(SListNode<T>* head) {
+    SListNode<T>* curr = head;
+    while (curr) {
+        SListNode<T>* nxt = curr->next;
+        delete(curr);
+        curr = nxt;
+    }
+}
 
 /**
  * Detects the presence of a cycle in a list.
