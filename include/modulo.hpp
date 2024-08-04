fb1ed818035204201f18f2852abf98c8b7e3785d
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-08-04 12:29:04 +0200

    Update modulo and add tests

--- include/modulo.hpp
+++ include/modulo.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
 #include <concepts>
+#include <stdexcept>
 
 namespace alg {
 
 /**
  * Computes a mod m.
- * In number theory, the result is guaranteed to be in the renge [0, m).
+ * The result is guaranteed to be in the renge [0, m), as in number theory.
  * In C++, when using % between operands of different sign, 
  * the sign of the result is implementation-defined.
- * To enforce the mathematical definition, we can force the result to be positive. 
+ * To enforce the mathematical definition, we force the result to be positive. 
  */
 template <std::integral T>
 T mod(T a, T m) {
+    if (m < 1) throw std::invalid_argument("Divisor must be >= 1.");
     T r = a % m;
     return (r < 0) ? (r + m) : r;
 }
