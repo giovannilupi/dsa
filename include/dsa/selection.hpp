4d0749027fd149ee38ad0ed34429f900d03c1ab0
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-08-10 12:30:54 +0200

    Refactor selection header

--- include/dsa/selection.hpp
+++ include/dsa/selection.hpp
@@ -19,8 +19,8 @@ namespace detail {
  * The problem has an inherent lower bound of n-1 comparisons.
  * Complexity: O(n)
  */
-template <SortableContainer TCont, typename Cmp>
-    requires std::predicate<Cmp, typename TCont::value_type, typename TCont::value_type>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 typename TCont::value_type extremeValue(const TCont& container, Cmp cmp) {
     if (container.empty()) throw std::invalid_argument("Container is empty");
     typename TCont::value_type res = *container.begin();
@@ -34,8 +34,8 @@ typename TCont::value_type extremeValue(const TCont& container, Cmp cmp) {
  * This sorts the container and returns the element at index k.
  * Complexity: O(n log n)
  */
-template <SortableContainer TCont, typename Cmp>
-    requires std::predicate<Cmp, typename TCont::value_type, typename TCont::value_type>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 typename TCont::value_type selectNthExtreme(TCont& container, std::size_t k, Cmp cmp) {
     if (container.empty()) throw std::invalid_argument("Container is empty");
     if (k >= container.size()) throw std::out_of_range("Index out of range");
@@ -46,7 +46,8 @@ typename TCont::value_type selectNthExtreme(TCont& container, std::size_t k, Cmp
 /**
  * Randomized quick sort partition function using the STL partition function.
  */
-template <SortableContainer TCont, typename Cmp>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 index partition(TCont& container, index low, index high, Cmp cmp) {
     // Pick a random element as pivot using a uniform distribution
     static std::mt19937 gen(std::random_device{}());
@@ -70,8 +71,8 @@ index partition(TCont& container, index low, index high, Cmp cmp) {
  * Average complexity: O(n)
  * Worst complexity: O(n^2)
  */
-template <SortableContainer TCont, typename Cmp>
-    requires std::predicate<Cmp, typename TCont::value_type, typename TCont::value_type>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 typename TCont::value_type quickSelectHelper(TCont& container, std::size_t k, Cmp cmp) {
     if (container.empty()) throw std::invalid_argument("Container is empty");
     if (k >= container.size()) throw std::out_of_range("Index out of range");
@@ -86,8 +87,8 @@ typename TCont::value_type quickSelectHelper(TCont& container, std::size_t k, Cm
     return container[low];
 }
 
-template <SortableContainer TCont, typename Cmp>
-    requires std::predicate<Cmp, typename TCont::value_type, typename TCont::value_type>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 typename TCont::value_type heapSelectHelper(TCont& container, std::size_t k, Cmp cmp) {
     if (container.empty()) throw std::invalid_argument("Container is empty");
     if (k >= container.size()) throw std::out_of_range("Index out of range");
@@ -105,8 +106,8 @@ typename TCont::value_type heapSelectHelper(TCont& container, std::size_t k, Cmp
  * Returns the nth extreme value in the container using the STL.
  * This runs in linear time.
  */
-template <SortableContainer TCont, typename Cmp>
-    requires std::predicate<Cmp, typename TCont::value_type, typename TCont::value_type>
+template <typename TCont, typename Cmp>
+    requires SortableContainer<TCont, Cmp>
 typename TCont::value_type selectModernHelper(TCont& container, std::size_t k, Cmp cmp) {
     if (container.empty()) throw std::invalid_argument("Container is empty");
     if (k >= container.size()) throw std::out_of_range("Index out of range");
@@ -120,7 +121,8 @@ typename TCont::value_type selectModernHelper(TCont& container, std::size_t k, C
 /**
  * Returns the smallest element in the container.
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type getMin(const T& container) {
     return detail::extremeValue(container, std::less<typename T::value_type>());
 }
@@ -128,7 +130,8 @@ typename T::value_type getMin(const T& container) {
 /**
  * Returns the largest element in the container.
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::greater<typename T::value_type>>
 typename T::value_type getMax(const T& container) {
     return detail::extremeValue(container, std::greater<typename T::value_type>());
 }
@@ -136,7 +139,9 @@ typename T::value_type getMax(const T& container) {
 /**
  * Naive algorithm for extracting minimum and maximum elements from the container.
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>> && 
+             SortableContainer<T, std::greater<typename T::value_type>>
 std::pair<typename T::value_type, typename T::value_type> getMinMax(const T& container) {
     return {getMin(container), getMax(container)};
 }
@@ -145,7 +150,9 @@ std::pair<typename T::value_type, typename T::value_type> getMinMax(const T& con
  * Optimized algorithm for extracting minimum and maximum elements from the container.
  * This reduces the number of comparisons from 2n-2 to 3(n/2).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>> && 
+             SortableContainer<T, std::greater<typename T::value_type>>
 std::pair<typename T::value_type, typename T::value_type> getMinMaxOpt(const T& container) {
     std::integral auto sz = container.size();
     if (!sz) throw std::invalid_argument("Container is empty");
@@ -169,7 +176,8 @@ std::pair<typename T::value_type, typename T::value_type> getMinMaxOpt(const T& 
  * Returns the kth smallest element in the container using the naive algorithm.
  * The value of k starts from 0 (which corresponds to the smallest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type selectNthMin(T& container, std::size_t k) {
     return detail::selectNthExtreme(container, k, std::less<typename T::value_type>());
 }
@@ -178,9 +186,9 @@ typename T::value_type selectNthMin(T& container, std::size_t k) {
  * Returns the kth largest element in the container using the naive algorithm.
  * The value of k starts from 0 (which corresponds to the largest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::greater<typename T::value_type>>
 typename T::value_type selectNthMax(T& container, std::size_t k) {
-
     return detail::selectNthExtreme(container, k, std::greater<typename T::value_type>());
 }
 
@@ -188,7 +196,8 @@ typename T::value_type selectNthMax(T& container, std::size_t k) {
  * Returns the nth smallest element in the container using the quick select algorithm.
  * The value of k starts from 0 (which corresponds to the smallest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type quickSelectMin(T& container, std::size_t k) {
     return detail::quickSelectHelper(container, k, std::less<typename T::value_type>());
 }
@@ -197,7 +206,8 @@ typename T::value_type quickSelectMin(T& container, std::size_t k) {
  * Returns the nth largest element in the container using the quick select algorithm.
  * The value of k starts from 0 (which corresponds to the largest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::greater<typename T::value_type>>
 typename T::value_type quickSelectMax(T& container, std::size_t k) {
     return detail::quickSelectHelper(container, k, std::greater<typename T::value_type>());
 }
@@ -206,7 +216,8 @@ typename T::value_type quickSelectMax(T& container, std::size_t k) {
  * Returns the nth smallest element in the container using the heap select algorithm.
  * The value of k starts from 0 (which corresponds to the smallest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type heapSelectMin(T& container, std::size_t k) {
     return detail::heapSelectHelper(container, k, std::greater<typename T::value_type>());
 }
@@ -215,7 +226,8 @@ typename T::value_type heapSelectMin(T& container, std::size_t k) {
  * Returns the nth largest element in the container using the heap select algorithm.
  * The value of k starts from 0 (which corresponds to the largest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::greater<typename T::value_type>>
 typename T::value_type heapSelectMax(T& container, std::size_t k) {
     return detail::heapSelectHelper(container, k, std::less<typename T::value_type>());
 }
@@ -224,7 +236,8 @@ typename T::value_type heapSelectMax(T& container, std::size_t k) {
  * Returns the kth smallest element in the container using the STL library's nth_element function.
  * The value of k starts from 0 (which corresponds to the smallest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type selectMinModern(T& container, std::size_t k) {
     return detail::selectModernHelper(container, k, std::less<typename T::value_type>());
 }
@@ -233,7 +246,8 @@ typename T::value_type selectMinModern(T& container, std::size_t k) {
  * Returns the kth smallest element in the container using the STL library's nth_element function.
  * The value of k starts from 0 (which corresponds to the largest element).
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::greater<typename T::value_type>>
 typename T::value_type selectMaxModern(T& container, std::size_t k) {
     return detail::selectModernHelper(container, k, std::greater<typename T::value_type>());
 }
@@ -242,7 +256,8 @@ typename T::value_type selectMaxModern(T& container, std::size_t k) {
  * Returns the median of the container using the STL library's nth_element function.
  * This runs in linear time.
  */
-template <SortableContainer T>
+template <typename T>
+    requires SortableContainer<T, std::less<typename T::value_type>>
 typename T::value_type getMedian(T& container) {
     auto sz = container.size();
     if (sz & 1) return selectMinModern(container, sz / 2);
