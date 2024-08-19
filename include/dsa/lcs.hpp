9210bd573dd760c6f6f155727b31d4765cc1af54
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-08-19 20:31:10 +0200

    Add longest common subsequence

---
+++ include/dsa/lcs.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <concepts>
+#include <optional>
+#include <string>
+#include <vector>
+#include "common.hpp"
+
+namespace alg {
+
+namespace detail {
+
+/**
+ * Helper function for the naive recursive implementation of the longest common subsequence.
+ */
+template <std::ranges::random_access_range T>
+    requires std::equality_comparable<std::ranges::range_value_t<T>>
+index lcsRecHelper(const T& c1, const T& c2, index i, index j) {
+    if (i == 0 || j == 0) return 0;
+    if (c1[i - 1] == c2[j - 1]) return 1 + lcsRecHelper(c1, c2, i - 1, j - 1);
+    return std::max(lcsRecHelper(c1, c2, i - 1, j), lcsRecHelper(c1, c2, i, j - 1));
+}
+
+/**
+ * Helper function for the top-down DP implementation of the longest common subsequence.
+ */
+template <std::ranges::random_access_range T>
+    requires std::equality_comparable<std::ranges::range_value_t<T>>
+index lcsMemoizedHelper(const T& c1, const T& c2, index i, index j, std::vector<std::vector<index>>& table) {
+    if (i == 0 || j == 0) return 0;
+    if (table[i][j] != -1) return table[i][j];
+    if (c1[i - 1] == c2[j - 1]) return table[i][j] = 1 + lcsMemoizedHelper(c1, c2, i - 1, j - 1, table);
+    return table[i][j] = std::max(lcsMemoizedHelper(c1, c2, i - 1, j, table), lcsMemoizedHelper(c1, c2, i, j - 1, table));
+}
+
+}
+
+/**
+ * Returns the length of the longest common subsequence of two containers.
+ * This uses the naive recursive implementation.
+ * Complexity: O(2^(min(n, m)))
+ */
+template <std::ranges::random_access_range T>
+    requires std::equality_comparable<std::ranges::range_value_t<T>>
+index lcsRec(const T& c1, const T& c2) {
+    return detail::lcsRecHelper(c1, c2, c1.size(), c2.size());
+}
+
+/**
+ * Returns the length of the longest common subsequence of two containers.
+ * This uses top-down dynamic programming.
+ * Complexity: O(n * m)
+ */
+template <std::ranges::random_access_range T>
+    requires std::equality_comparable<std::ranges::range_value_t<T>>
+index lcsMemoized(const T& c1, const T& c2) {
+    auto sz1 = c1.size();
+    auto sz2 = c2.size();
+    std::vector<std::vector<index>> table(sz1 + 1, std::vector<index>(sz2 + 1, -1));
+    return detail::lcsMemoizedHelper(c1, c2, c1.size(), c2.size(), table);
+}
+
+/**
+ * Returns the length of the longest common subsequence of two containers.
+ * This uses bottom-up dynamic programming.
+ * Notice we only need to store the last row of the table.
+ * Complexity: O(n * m)
+ */
+template <std::ranges::random_access_range T>
+    requires std::equality_comparable<std::ranges::range_value_t<T>>
+index lcsDP(const T& c1, const T& c2) {
+    auto sz1 = c1.size();
+    auto sz2 = c2.size();
+    std::vector<index> table(sz2 + 1, 0);
+    for (index i = 1; i <= sz1; ++i) {
+        // We may need to access table[j-1], which is overwritten by the previous iteration
+        index prev = 0;
+        for (index j = 1; j <= sz2; ++j) {
+            index curr = table[j];
+            table[j] = (c1[i - 1] == c2[j - 1]) 
+                ? 1 + prev 
+                : std::max(table[j], table[j - 1]);
+            prev = curr;
+        }
+    }
+    return table[sz2];
+}
+
+}
---
+++ tests/lcs_tests.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "common.hpp"
+#include "lcs.hpp"
+
+using LCSFunc = std::function<alg::index(const std::string&, const std::string&)>;
+using ::testing::Combine;
+using ::testing::ValuesIn;
+
+static std::map<std::string, LCSFunc> lcsFunctions = {
+    {"LCSRecursive", alg::lcsRec<std::string>},
+    {"LCSMemoization", alg::lcsMemoized<std::string>},
+    {"LCSTabulation", alg::lcsDP<std::string>},
+};
+
+static std::map<std::string, std::pair<std::pair<std::string, std::string>, alg::index>> testStrings = {
+    {"EmptyStrings", {{ "", "" }, 0}},
+    {"OneEmptyString", {{ "ABCDE", "" }, 0}},
+    {"CompletelyDifferent", {{ "ABC", "DEF" }, 0}},
+    {"SingleCharacterMatch", {{ "A", "A" }, 1}},
+    {"SingleCharacterNoMatch", {{ "A", "B" }, 0}},
+    {"Simple", {{ "abcba", "abcbcba" }, 5}},
+    {"CaseSensitiveMatch", {{ "abc", "ABC" }, 0}},
+    {"RepeatedCharacters", {{ "AAAA", "AA" }, 2}},
+    {"NonContiguousMatch", {{ "axbycz", "abc" }, 3}},
+    {"DifferentRepeatedCharacters", {{ "AABBAABB", "ABAB" }, 4}},
+    {"IdenticalStrings", {{ "SAMESTRING", "SAMESTRING" }, 10}},
+};
+
+class LCSTest :
+    public ::testing::TestWithParam<std::tuple<
+    std::pair<const std::string, LCSFunc>,
+    std::pair<const std::string, std::pair<std::pair<std::string, std::string>, alg::index>>>>
+{};
+
+TEST_P(LCSTest, WorksWithAllInputs) {
+    // Get the parameters for the current test case
+    auto param = GetParam();
+    auto lcsFunc = std::get<0>(param).second;
+    auto strings = std::get<1>(param).second;
+    auto s1 = strings.first.first;
+    auto s2 = strings.first.second;
+    auto expected = strings.second;
+    
+    EXPECT_EQ(lcsFunc(s1, s2), expected);
+}
+
+
+INSTANTIATE_TEST_SUITE_P(LCSTestsGenerator, LCSTest,
+    Combine(ValuesIn(lcsFunctions), ValuesIn(testStrings)),
+    [](const testing::TestParamInfo<LCSTest::ParamType>& info) {
+        return std::get<0>(info.param).first + "_" + std::get<1>(info.param).first;
+    });
