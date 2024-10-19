499519445796a3677be55c981ca0d5d7d170a088
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-10-19 20:13:52 +0200

    Add weighted graph algorithms

---
+++ include/dsa/graph_weighted.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <algorithm>
+#include <limits>
+#include <queue>
+#include <tuple>
+#include <vector>
+#include "union_find.hpp"
+
+
+namespace alg {
+
+/**
+ * Computes the minimum spanning tree weight of the given graph using Kruskal's algorithm.
+ * The algorithm applies to unidrected, connected, weighted graphs.
+ * If the graph is not connected, the behavior is undefined.
+ */
+inline int mstKruskal(const std::vector<std::vector<std::pair<int, int>>>& adjList) {
+    int n = adjList.size();
+    // Collect edges
+    std::vector<std::tuple<int, int, int>> edges;
+    for (int u = 0; u != n; ++u) {
+        for (auto &[v, weight] : adjList[u]) {
+            // Avoid duplicate edges, given the graph is undirected
+            if (u < v) edges.emplace_back(weight, u, v);
+        }
+    }
+    // Sort edges
+    std::sort(edges.begin(), edges.end());
+    // Setup
+    alg::UnionFind uf(n);
+    int mstWeight = 0;
+    int mstEdges = 0;
+    // Process edges in order
+    for (auto &[weight, u, v] : edges) {
+        if (uf.find(u) != uf.find(v)) {
+            uf.unite(u, v);
+            mstWeight += weight;
+            if (++mstEdges == n - 1) break;
+        }
+    }
+    return mstWeight;
+}
+
+/**
+ * Computes the minimum spanning tree weight of the given graph using Prim's algorithm.
+ * The algorithm applies to unidrected, connected, weighted graphs.
+ * If the graph is not connected, the behavior is undefined.
+ */
+inline int mstPrim(const std::vector<std::vector<std::pair<int, int>>>& adjList) {
+    int n = adjList.size();
+    // Tracks nodes included in the MST
+    std::vector<bool> visited(n, false);
+    // Stores minimum weight edge connecting each node to the MST
+    std::vector<int> dist(n, std::numeric_limits<int>::max());
+    // Efficiently extract the minimum weight edge
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+    // Start algorithm from source node 0
+    pq.emplace(0, 0); 
+    dist[0] = 0;
+    int mstWeight = 0;
+    while (!pq.empty()) {
+        auto [w, u] = pq.top();
+        pq.pop();
+        // Outdated edge
+        if (visited[u]) continue;
+        visited[u] = true;
+        mstWeight += w;
+        // Add each outgoing edge to the priority queue
+        for (auto &[v, weight] : adjList[u]) {
+            // Do not add outdated edges 
+            if (!visited[v] && dist[v] > weight) {
+                dist[v] = weight;
+                pq.emplace(weight, v);
+            }
+        }
+    }
+    return mstWeight;
+}
+
+/**
+ * Computes the shortest path from the source node to all other nodes using Dijkstra's algorithm.
+ * The algorithm applies to directed, weighted graphs with positive edge weights.
+ * The result is returned as a vector of shortest distances to each node.
+ * If a node is unreachable from the source, the distance is set to std::numeric_limits<int>::max().
+ */
+inline std::vector<int> djikstra(const std::vector<std::vector<std::pair<int, int>>>& adjList, int source) {
+    int n = adjList.size();
+    // Stores shortest distance to each node from the source
+    std::vector<int> dist(n, std::numeric_limits<int>::max());
+    dist[source] = 0;
+    // Min-heap setup
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+    pq.emplace(0, source);
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if (d > dist[u]) continue;
+        for (auto &[v, weight] : adjList[u]) {
+            // Relax edge (u, v)
+            if (d + weight < dist[v]) {
+                dist[v] = d + weight;
+                pq.emplace(dist[v], v);
+            }
+        }
+    }
+    return dist;    
+} 
+
+} // namespace alg
---
+++ tests/graph_weighted_tests.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include "graph_weighted.hpp"
+
+using namespace alg;
+
+TEST(GraphTest, MSTWork) {
+    // Graph with distinct weights, hence a unique MST
+    std::vector<std::vector<std::pair<int, int>>> graph = {
+        {{1, 1}, {2, 2}},   // Node 0: edges to 1 (weight 1), 2 (weight 2)
+        {{0, 1}, {2, 3}},   // Node 1: edges to 0 (weight 1), 2 (weight 3)
+        {{0, 2}, {1, 3}}    // Node 2: edges to 0 (weight 2), 1 (weight 3)
+    };
+    // Expected MST: (0-1, weight 1), (0-2, weight 2) -> Total weight = 3
+    EXPECT_EQ(mstKruskal(graph), 3);
+    EXPECT_EQ(mstPrim(graph), 3);
+    
+    // Graph with duplicate weights, hence multiple MSTs can exist
+    graph = {
+        {{1, 1}, {2, 2}, {3, 3}},  // Node 0: edges to 1 (weight 1), 2 (weight 2), 3 (weight 3)
+        {{0, 1}, {3, 2}},              // Node 1: edges to 0 (weight 1), 3 (weight 2)
+        {{0, 2}, {3, 2}},              // Node 2: edges to 0 (weight 2), 3 (weight 2)
+        {{0, 3}, {1, 2}, {2, 2}}   // Node 3: edges to 0 (weight 3), 1 (weight 2), 2 (weight 2)
+    };
+    // MST1: (0-1, weight 1), (1-3, weight 2), (2-3, weight 2) -> Total weight = 5
+    // MST2: (0-1, weight 1), (1-3, weight 2), (0-2, weight 2) -> Total weight = 5
+    EXPECT_EQ(mstKruskal(graph), 5);
+    EXPECT_EQ(mstPrim(graph), 5);
+}
+
+TEST(GraphTest, DjikstraWorks) {
+    // Graph with single node and no edges
+    std::vector<std::vector<std::pair<int, int>>> graph = {
+        {}   // Node 0 has no edges
+    };
+    std::vector<int> expected = {0};
+    ASSERT_EQ(djikstra(graph, 0), expected);
+
+    // Graph with unreachable node
+    graph = {
+        {{1, 2}},    // Node 0: edges to 1 (weight 2)
+        {{0, 2}},    // Node 1: edges to 0 (weight 2)
+        {{}}         // Node 2: disconnected
+    };
+    
+    expected = {0, 2, std::numeric_limits<int>::max()};
+    ASSERT_EQ(djikstra(graph, 0), expected);
+
+    // Connected graph
+    graph = {
+        {{1, 4}, {2, 1}},              // Node 0: edges to 1 (weight 4), 2 (weight 1)
+        {{0, 4}, {2, 2}, {3, 1}},  // Node 1: edges to 0 (weight 4), 2 (weight 2), 3 (weight 1)
+        {{0, 1}, {1, 2}, {3, 5}},  // Node 2: edges to 0 (weight 1), 1 (weight 2), 3 (weight 5)
+        {{1, 1}, {2, 5}}               // Node 3: edges to 1 (weight 1), 2 (weight 5)
+    };
+    expected = {0, 3, 1, 4};
+    ASSERT_EQ(djikstra(graph, 0), expected);
+}
