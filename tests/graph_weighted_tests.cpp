c02b681f24fd596ea58eb71524340d2d7d7431cb
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2025-05-02 13:13:13 +0200

    Minor changes to graph tests

--- tests/graph_weighted_tests.cpp
+++ tests/graph_weighted_tests.cpp
@@ -14,8 +14,8 @@ TEST(GraphTest, MSTWork) {
         {{0, 2}, {1, 3}}    // Node 2: edges to 0 (weight 2), 1 (weight 3)
     };
     // Expected MST: (0-1, weight 1), (0-2, weight 2) -> Total weight = 3
-    EXPECT_EQ(alg::mstKruskal(graph), 3);
-    EXPECT_EQ(alg::mstPrim(graph), 3);
+    EXPECT_EQ(mstKruskal(graph), 3);
+    EXPECT_EQ(mstPrim(graph), 3);
     
     // Graph with duplicate weights, hence multiple MSTs can exist
     graph = {
@@ -26,8 +26,8 @@ TEST(GraphTest, MSTWork) {
     };
     // MST1: (0-1, weight 1), (1-3, weight 2), (2-3, weight 2) -> Total weight = 5
     // MST2: (0-1, weight 1), (1-3, weight 2), (0-2, weight 2) -> Total weight = 5
-    EXPECT_EQ(alg::mstKruskal(graph), 5);
-    EXPECT_EQ(alg::mstPrim(graph), 5);
+    EXPECT_EQ(mstKruskal(graph), 5);
+    EXPECT_EQ(mstPrim(graph), 5);
 }
 
 TEST(GraphTest, DjikstraWorks) {
@@ -36,7 +36,7 @@ TEST(GraphTest, DjikstraWorks) {
         {}   // Node 0 has no edges
     };
     std::vector<int> expected = {0};
-    ASSERT_EQ(alg::djikstra(graph, 0), expected);
+    ASSERT_EQ(djikstra(graph, 0), expected);
 
     // Graph with unreachable node
     graph = {
@@ -46,7 +46,7 @@ TEST(GraphTest, DjikstraWorks) {
     };
     
     expected = {0, 2, std::numeric_limits<int>::max()};
-    ASSERT_EQ(alg::djikstra(graph, 0), expected);
+    ASSERT_EQ(djikstra(graph, 0), expected);
 
     // Connected graph
     graph = {
@@ -56,7 +56,7 @@ TEST(GraphTest, DjikstraWorks) {
         {{1, 1}, {2, 5}}               // Node 3: edges to 1 (weight 1), 2 (weight 5)
     };
     expected = {0, 3, 1, 4};
-    ASSERT_EQ(alg::djikstra(graph, 0), expected);
+    ASSERT_EQ(djikstra(graph, 0), expected);
 }
 
 }  // namespace
